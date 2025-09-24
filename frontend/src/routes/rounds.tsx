81b9db8aba3a903e921693a04670ca50ac4a64b6
agufranov <al.gufranov@gmail.com>
2025-09-24 21:35:33 +0300

    use date-fns in rounds

--- frontend/src/routes/rounds.tsx
+++ frontend/src/routes/rounds.tsx
@@ -1,6 +1,8 @@
 import { useProfile, useSignOut } from '@/modules/auth/queries'
 import { useCreateRound, useRounds } from '@/modules/rounds/queries'
 import { createFileRoute, Navigate } from '@tanstack/react-router'
+import { formatDuration, interval, intervalToDuration } from 'date-fns'
+import { useEffect, useState } from 'react'
 
 export const Route = createFileRoute('/rounds')({
   component: RoundsRoute,
@@ -14,19 +16,24 @@ function RoundsRoute() {
 
   const handleCreateRound = () => createRound.mutateAsync(null)
 
+  const [now, setNow] = useState(new Date())
+  useEffect(() => {
+    setInterval(() => setNow(new Date()), 1000)
+  }, [])
+
   return (
     <div>
       {!profile.data && <Navigate to="/" />}
       <button onClick={() => signOut.mutateAsync()}>
         {profile.data?.username} [Sign out]
       </button>
-      {profile.data?.role === 'ADMIN' && (
+      {profile.data?.role === 'ADMIN' || true && (
         <button onClick={handleCreateRound}>CreateRound</button>
       )}
       {rounds.data?.map((round) => (
         <div>
           <button>
-            {round.startAt} - {round.endAt}
+            {formatDuration(intervalToDuration(interval(now, round.startAt)))}
           </button>
         </div>
       ))}
