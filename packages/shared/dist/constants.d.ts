23dfb3dde2e41bd8d2502dad7f7e751780a949bf
agufranov <al.gufranov@gmail.com>
2025-09-28 16:55:28 +0300

    remove dist

--- packages/shared/dist/constants.d.ts
+++
@@ -1,6 +0,0 @@
-import { Role } from "@prisma/client";
-export declare const AUTH_COOKIE_NAME = "sessionId";
-export declare const SPECIAL_ROLES: Record<string, Role>;
-export declare const SKIP_ROUTE_HOOKS: {
-    auth: string[];
-};
